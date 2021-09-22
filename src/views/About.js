156402e2cb543315d69ac3811687066e6c5b74cf
macktrain <82425068+macktrain@users.noreply.github.com>
2021-09-22 15:49:34 -0600

    About ready

--- src/views/About.js
+++ src/views/About.js
@@ -1,11 +1,62 @@
 //rafce
 import React from 'react'
-import Header from '../components/Header'
 
 const About = () => {
     return (
-        <div>
-            About Me
+        <div id='about'>
+            <div id='aboutIntro'>
+                As an experienced development professional with 10 years of development experience, 15 years leadership/management and 24 years of overall IT experience, 
+                I have learned the art of delivering results in a collaborative manner. Being a team player who encourages innovation, driving for "next level" solutions 
+                and places value on the individual is key to creating an environment conducive to productivity and is key to building up a team that drives for best of 
+                class business value in a timely and cost effective manner.  While my resume will show the following experience:
+            </div>
+            <div id='aboutHighlights'>
+                <ul>
+                    <li>10 years – Application Development.</li>
+                    <li>15 years – Technology Leadership.</li>
+                    <ul>
+                        <li>Upwards of 12 direct reports.</li>
+                        <li>I have successfully managed development resources in both India and Germany.</li>
+                        <li>I have successfully managed US-based resources both local and remote.</li>
+                    </ul>
+                    <li>10 years – Technology Product Ownership.</li>
+                    <li>9 years –Project Management.</li>
+                    <ul>
+                        <li>Project managed worldwide, database-driven applications through the full software development lifecycle.</li>
+                    </ul>
+                    <li>Certified Agile Scrum Master – not maintained.</li>
+                    <li>Certified Agile Product Owner – not maintained.</li>
+                    <li>24 years – Overall Industry Experience.</li>
+                    <ul>
+                        <li>Technical management/strategy, project management, people management, vendor management, customer relationship management, software development and product technical support.</li>
+                    </ul>
+                </ul>
+            </div>
+            <div id='aboutIntangibles'>
+                <i>What my resume may not convey are my: </i>
+                <ul>
+                    <li>Work ethic. </li>
+                    <li>Servant leadership. </li>
+                    <li>Commitment to delivering results. </li>
+                    <li>Ability to recognize strength in others, motivate them and pull out their best. </li>
+                    <li>Ability to be excessively comfortable with ambiguity. </li>
+                    <li>Influencing skills and, more importantly, my openness to being influenced. </li>
+                    <li>Philosophy of listening first. </li>
+                </ul>
+            </div>
+            <div id='aboutClosing'>
+                If you have an exciting opportunity where I can leverage my tangible and intangible skills in helping to build out strong, 
+                supportable, scalable and useful solutions for your organization, please reach out to me ... I look forward to speaking with you!
+            </div>
+            <br/>
+            <div id='signature'>
+                <div>
+                    Best Regards,
+                </div>
+                <div>
+                    Lee Macklin
+                </div>
+            </div>
         </div>
     )
 }
