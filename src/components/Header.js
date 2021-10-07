724a4c52acb29a21a01de47063526fbf47fdd3b6
macktrain <82425068+macktrain@users.noreply.github.com>
2021-10-06 18:19:30 -0600

    final fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,21 +6,21 @@ import {LinkContainer} from "react-router-bootstrap"
 const Header = (props) => {
     //Everything within the return is jsx and vars need {} for a jsx variable and expressions (.map, if-then, etc)
     return (
-        <Navbar collapseOnSelect bg="dark" variant="dark" expand="sm" sticky="top">
-            <Container>
-                <Navbar.Brand><img id='nameLogo' src={nameLogo} alt='Signature' /></Navbar.Brand>
-                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="ml-auto nav-links-custom">
-                        <LinkContainer to="/"><Nav.Link>Home</Nav.Link></LinkContainer>
-                        <LinkContainer to="/about"><Nav.Link>About</Nav.Link></LinkContainer>
-                        <LinkContainer to="/portfolio"><Nav.Link>Portfolio</Nav.Link></LinkContainer>
-                        <LinkContainer to="/contact"><Nav.Link>Contact</Nav.Link></LinkContainer>
-                        <LinkContainer to="/resume"><Nav.Link>Resume</Nav.Link></LinkContainer>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
+            <Navbar collapseOnSelect bg="dark" variant="dark" expand="sm" sticky="top">
+                <Container>
+                    <Navbar.Brand><img id='nameLogo' src={nameLogo} alt='Signature' /></Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="basic-navbar-nav">
+                        <Nav className="ml-auto nav-links-custom">
+                            <LinkContainer to="/"><Nav.Link>Home</Nav.Link></LinkContainer>
+                            <LinkContainer to="/about"><Nav.Link>About</Nav.Link></LinkContainer>
+                            <LinkContainer to="/portfolio"><Nav.Link>Portfolio</Nav.Link></LinkContainer>
+                            <LinkContainer to="/contact"><Nav.Link>Contact</Nav.Link></LinkContainer>
+                            <LinkContainer to="/resume"><Nav.Link>Resume</Nav.Link></LinkContainer>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
     )
 }
 
